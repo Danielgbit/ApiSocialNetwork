842170f25dd6c9ad4e3c124ac28b0a3fee73a86d
danielgbit <danielbitmobb@gmail.com>
2024-10-02 12:45:55 -0500

    fix post

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const url = `https://jsonplaceholder.typicode.com/posts`;
 
 let isLoading = true;
+let posts = [];
+//States ---
 
-const posts = [];
-
-async function getData(newPost) {
+async function getData() {
     const res = await fetch(url);
 
     if (!res.ok) {
@@ -15,21 +15,14 @@ async function getData(newPost) {
     isLoading = false;
 
     const data = await res.json();
-
     const limitedUsers = data.slice(0, 5);
-    posts.push(limitedUsers);
+    posts.push(...limitedUsers);
 
-    if (newPost && Object.keys(newPost).length > 0) {
-        limitedUsers.push(newPost);
-        posts.push(newPost);
-    }
-
-    limitedUsers.forEach((data) => {
+    posts.forEach((data) => {
         renderPostList(data)
     });
-
-};
-
+ 
+ };
 
 const renderData = document.querySelector('.render-data');
 
@@ -37,25 +30,28 @@ function renderPostList(data) {
     const listItems = document.createElement('li');
     listItems.classList.add('.list-items');
 
+    console.log(data);
+    
+
 
     if (isLoading) {
-        listItems.innerHTML = `loading`;
+        listItems.innerHTML = `loading`; //Loading
 
     } else {
         listItems.innerHTML = `
             <strong>${data.title}</strong>
             <p>${data.body}</p>
             <div class='container-buttons-updatePost' >
-                <button onClick="editPost()">Edit post</button>
+                <button onClick="editPostToggle(${data.id})">Edit post</button>
                 <button deletePost(${data.id})>Delete post</button>     
             </div>
     
             <section class="add-post-container update-post-container">
                 <h3>update post</h3>
                 <form>
-                    <input type="text" id="titleUpdate" placeholder="Add title">
-                    <textarea type="text" placeholder="Add descriptionUpdate" id="descriptionUpdate" maxlength="30" minlength="5"></textarea>
-                    <button onclick="putData(event, ${data.id})">Send post</button>
+                    <input type="text" id="title-update" placeholder="Add title">
+                    <textarea type="text" placeholder="Add description" id="description-update" maxlength="30" minlength="5"></textarea>
+                    <button onclick="updateData(event, ${data.id})">Send post</button>
                 </form>
                 <button onClick="closeForm()">Close form</button>
             </section>
@@ -68,7 +64,9 @@ function renderPostList(data) {
 
 
 function emptyPosts() {
-    return renderData.innerHTML = ''
+    renderData.innerHTML = ''
+    posts = [];
+    return
 };
 
 const form = document.querySelector('form').addEventListener('click', (e) => {
@@ -98,49 +96,39 @@ async function postData() {
 
     const data = await res.json();
 
-    await getData(data);
+    if (Object.keys(data).length === 0) {
+        alert('error creating post');
+    }else {
+        isLoading = false;
+        renderPostList(data);
+        console.log(data);
+        
+    };
 };
 
 
-function closeForm() {
-    const putFormPost = document.querySelector('.update-post-container');
-    putFormPost.style.display = 'none';
-}
+const editPostToggle = () => { 
+    const seccionUpdateForm = document.querySelector('.update-post-container');
+        if (seccionUpdateForm.style.display = 'none') {
+            seccionUpdateForm.style.display = 'block'
+        }else {
+            seccionUpdateForm.style.display = 'none'
+        };
+} //Visibility
 
 
-function editPost(id) {
-    const putFormPost = document.querySelector('.update-post-container');
-    putFormPost.style.display = 'block';
-};
-
-async function putData(e, id) {
+function updateData(e, id){
     e.preventDefault();
 
-    const title = document.querySelector('#titleUpdate');
-    const description = document.querySelector('#descriptionUpdate');
+    const titleUpdate = document.querySelector('#title-update');
+    const descriptionUpdate = document.querySelector('#description-update');
 
 
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            id: id,
-            title: title.value,
-            body: description.value,
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
+    if (titleUpdate.value.trim() === '' || descriptionUpdate.value.trim() === '') {
+        alert('added the fields');
+    };
 
-    const data = await res.json();
-/* 
-    const indexPost = posts && posts[0].findIndex((item) =>  item.id === data.id );
+    console.log(id);
+    
 
-    if (indexPost != -1) {
-        posts[indexPost] = data;
-    }else {
-        alert('error al encontrar indice');
-    }; */
-
-}
+};
